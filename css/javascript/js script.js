d0c40757801f9e854d443c7cca79e8aa7d12989d
Rahul <rahulkavitha666@gmail.com>
2025-06-14 16:12:26 +0530

    commit message

---
+++ css/javascript/js script.js
@@ -0,0 +1,23 @@
+var a="40"
+var b="20"
+console.log (a*b);
+console.log(typeof a)
+console.log(typeof b)
+var mark=80;
+var result=(mark>=80)? "O":(mark>=60)?"A+":(mark>35)?"P":"F"
+console.log(result)
+
+
+
+let arr =[10,20,30,40];
+let arr2 =[...arr,50,60,70]
+console.log(arr)
+console.log(arr2)
+
+var marks =[90,86,94,100,78]
+var [m1,m2,m3,m4,m5] = marks;
+console.log(m1)
+console.log(m2)
+console.log(m3)
+console.log(m4)
+console.log(m5)
